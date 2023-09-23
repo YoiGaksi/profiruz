80b5622726cd1b434ba458ddf216d097b4d75639
YoiGaksi <86912365+YoiGaksi@users.noreply.github.com>
2023-09-23 09:13:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ // Mengambil referensi tombol
+        const toIG = document.getElementById('toIG');
+
+        // Menambahkan event listener untuk mengarahkan pengguna saat tombol diklik
+        toIG.addEventListener('click', function() {
+            // Ganti URL dengan URL halaman yang ingin Anda tuju
+            window.location.href = 'https://www.instagram.com/ocrystalineo/'; 
+        });
+        const toTT = document.getElementById('toTT');
+
+        // Menambahkan event listener untuk mengarahkan pengguna saat tombol diklik
+        toTT.addEventListener('click', function() {
+            // Ganti URL dengan URL halaman yang ingin Anda tuju
+            window.location.href = 'https://www.tiktok.com/@icrystallinei'; 
+        });
+        const hobi = document.getElementById('hobi');
+
+        // Menambahkan event listener untuk mengarahkan pengguna saat tombol diklik
+        hobi.addEventListener('click', function() {
+            // Ganti URL dengan URL halaman yang ingin Anda tuju
+            window.location.href = 'hobi.html'; 
+        });
